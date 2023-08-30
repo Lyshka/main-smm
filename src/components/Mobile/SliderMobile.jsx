d897d9b8b8682b4bdac8be9ffec2d2056d960ce2
Lyshka <lesha_novitskiy@mail.ru>
2023-08-30 12:10:14 +0300

    change
    change slide

--- src/components/Mobile/SliderMobile.jsx
+++ src/components/Mobile/SliderMobile.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from "@material-tailwind/react";
 import { useEffect, useState } from "react";
 
-import ModalSliderMobile from "./ModalSliderMobile"
+import ModalSliderMobile from "./ModalSliderMobile";
 
 const SliderMobile = () => {
   let a = null;
@@ -58,32 +58,30 @@ const SliderMobile = () => {
       >
         <div className="relative w-screen h-[calc(100vh-90px)] duration-500 text-black flex justify-center items-center">
           <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/04/1-Mobile.webp"
+            src="https://smm-ibis.by/wp-content/uploads/2023/08/Frame-331.png"
             className="w-screen h-full relative"
             alt=""
           />
 
-          <div className="absolute p-6 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/20 w-[330px]">
+          <div className="absolute p-6 rounded-2xl flex justify-start items-start flex-col gap-y-5 bg-black/20 w-[330px]">
             <div className="w-60">
               <span className="font-light text-white leading-4 text-sm uppercase">
                 Ибис маркетинг
               </span>
-              <h1 className="leading-8 text-[26px] text-white">
-                Создание и<br /> продвижение сайтов
+              <h1 className="leading-8 text-white text-lg">
+                Маркетинговый отдел на аутсорсе
               </h1>
             </div>
+            <p className="text-white leading-6 tracking-[1%] text-xs">
+              Уже Kia, JAC и JETOUR выбрали нашу услугу для своего успеха на
+              аутсорсе, всего за 1100 BYN. Начнем прямо сейчас?
+            </p>
             <div className="flex justify-center w-full items-center flex-col gap-y-4">
-              <a className="w-full" href="https://smm-ibis.by/site-creation/">
-                <button className="px-4 py-2 bg-white w-full rounded-[100px] text-xs leading-6 uppercase">
-                  Узнать больше
-                </button>
-              </a>
-
               <button
                 onClick={handleModal}
                 className="px-4 py-2 text-white w-full bg-[#FF3A2D] rounded-[100px] text-xs leading-6 uppercase"
               >
-                Начать продвижение
+                Оставить заявку
               </button>
             </div>
           </div>
@@ -133,107 +131,30 @@ const SliderMobile = () => {
         </div>
         <div className="relative w-screen h-[calc(100vh-90px)] duration-500 text-black flex justify-center items-center">
           <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/04/2-Mobile.webp"
+            src="https://smm-ibis.by/wp-content/uploads/2023/08/Frame-330.png"
             className="w-screen h-full relative"
             alt=""
           />
 
-          <div className="absolute p-6 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/20 w-[330px]">
+          <div className="absolute p-6 rounded-2xl flex justify-start items-start flex-col gap-y-5 bg-black/20 w-[330px]">
             <div className="w-60">
               <span className="font-light text-white leading-4 text-sm uppercase">
                 Ибис маркетинг
               </span>
-              <h1 className="leading-8 text-[26px] text-white">
-                Создание и<br /> продвижение сайтов
+              <h1 className="leading-8 text-lg text-white">
+                Пора заставить Ваш бизнес расти!
               </h1>
             </div>
+            <p className="text-white leading-6 tracking-[1%] text-xs">
+              Таргетированная реклама + ведение социальных сетей всего за 450
+              BYN
+            </p>
             <div className="flex justify-center w-full items-center flex-col gap-y-4">
-              <a className="w-full" href="https://smm-ibis.by/site-creation/">
-                <button className="px-4 py-2 bg-white w-full rounded-[100px] text-xs leading-6 uppercase">
-                  Узнать больше
-                </button>
-              </a>
-
               <button
                 onClick={handleModal}
                 className="px-4 py-2 text-white w-full bg-[#FF3A2D] rounded-[100px] text-xs leading-6 uppercase"
               >
-                Начать продвижение
-              </button>
-            </div>
-          </div>
-
-          <div className="flex w-full justify-between items-center absolute px-2">
-            <div
-              onClick={() => a()}
-              className="w-8 h-8 cursor-pointer rounded-full flex justify-center items-center bg-black/50"
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 12L6 8L10 4"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-            <div
-              onClick={() => b()}
-              className="w-8 h-8 rounded-full cursor-pointer flex justify-center items-center bg-black/50"
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6 4L10 8L6 12"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div className="relative w-screen h-[calc(100vh-90px)] duration-500 text-black flex justify-center items-center">
-          <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/04/3-Mobile.webp"
-            className="w-screen h-full relative"
-            alt=""
-          />
-
-          <div className="absolute p-6 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/20 w-[330px]">
-            <div className="w-60">
-              <span className="font-light text-white leading-4 text-sm uppercase">
-                Ибис маркетинг
-              </span>
-              <h1 className="leading-8 text-[26px] text-white">
-                Создание и<br /> продвижение сайтов
-              </h1>
-            </div>
-            <div className="flex justify-center w-full items-center flex-col gap-y-4">
-              <a className="w-full" href="https://smm-ibis.by/site-creation/">
-                <button className="px-4 py-2 bg-white w-full rounded-[100px] text-xs leading-6 uppercase">
-                  Узнать больше
-                </button>
-              </a>
-
-              <button
-                onClick={handleModal}
-                className="px-4 py-2 text-white w-full bg-[#FF3A2D] rounded-[100px] text-xs leading-6 uppercase"
-              >
-                Начать продвижение
+                Оставить заявку
               </button>
             </div>
           </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -57,36 +57,30 @@ const Slider = () => {
       >
         <div className="relative min-w-max h-[calc(100vh-96px)] duration-500 text-black flex justify-start items-center">
           <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/04/5314559233188965801.webp"
+            src="https://smm-ibis.by/wp-content/uploads/2023/08/Баннер-1.png"
             className="w-screen h-full relative"
             alt=""
           />
 
-          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/20 w-[749px]">
+          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/[0.15] w-[749px]">
             <div>
               <span className="font-light text-white leading-8 text-xl uppercase">
                 Ибис маркетинг
               </span>
               <h1 className="leading-[58px] text-[50px] text-white">
-                Создание и<br /> продвижение сайтов
+                Маркетинговый отдел на аутсорсе
               </h1>
             </div>
-            {/* <p className="text-white leading-6 tracking-[1%]">
-            Создание сайтов полного цикла, от разработки карты сайта, до
-            продвижения и внедрения новых возможностей!
-          </p> */}
+            <p className="text-white leading-6 tracking-[1%]">
+              Уже Kia, JAC и JETOUR выбрали нашу услугу для своего успеха на
+              аутсорсе, всего за 1100 BYN. Начнем прямо сейчас?
+            </p>
             <div className="flex justify-center items-center gap-x-8">
-              <a href="https://smm-ibis.by/site-creation/">
-                <button className="px-6 py-4 bg-white rounded-[100px] text-lg leading-6 uppercase">
-                  Узнать больше
-                </button>
-              </a>
-
               <button
                 onClick={handleModal}
                 className="px-6 py-4 text-white bg-[#FF3A2D] rounded-[100px] text-lg leading-6 uppercase"
               >
-                Начать продвижение
+                Оставить заявку
               </button>
             </div>
           </div>
@@ -136,107 +130,30 @@ const Slider = () => {
         </div>
         <div className="relative min-w-max h-[calc(100vh-96px)] duration-500 text-black flex justify-start items-center">
           <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/04/5314342852736594003.webp"
+            src="https://smm-ibis.by/wp-content/uploads/2023/08/Баннер-1-1.png"
             className="w-screen relative h-full"
             alt=""
           />
 
-          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/20 w-[749px]">
+          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/[0.15] w-[749px]">
             <div>
-              <span className="font-light text-white leading-8 text-xl uppercase">
+              <span className="font-light text-[#333333] leading-8 text-xl uppercase">
                 Ибис маркетинг
               </span>
-              <h1 className="leading-[58px] text-[50px] text-white">
-                SMM-продвижение
+              <h1 className="leading-[58px] text-[50px] text-[#333333]">
+                Пора заставить Ваш бизнес расти!
               </h1>
             </div>
+            <p className="text-white leading-6 tracking-[1%] text-[#333333]">
+              Таргетированная реклама + ведение социальных сетей всего за 450
+              BYN
+            </p>
             <div className="flex justify-center items-center gap-x-8">
-              <a href="https://smm-ibis.by/smmprodvizheniye/">
-                <button className="px-6 py-4 bg-white rounded-[100px] text-lg leading-6 uppercase">
-                  Узнать больше
-                </button>
-              </a>
-
               <button
                 onClick={handleModal}
                 className="px-6 py-4 text-white bg-[#FF3A2D] rounded-[100px] text-lg leading-6 uppercase"
               >
-                Начать продвижение
-              </button>
-            </div>
-          </div>
-
-          <div className="flex w-full justify-between items-center absolute px-[238px]">
-            <div
-              onClick={() => a()}
-              className="w-8 h-8 cursor-pointer rounded-full flex justify-center items-center bg-black/50"
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 12L6 8L10 4"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-            <div
-              onClick={() => b()}
-              className="w-8 h-8 rounded-full cursor-pointer flex justify-center items-center bg-black/50"
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6 4L10 8L6 12"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div className="relative min-w-max h-[calc(100vh-96px)] duration-500 text-black flex justify-start items-center">
-          <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/04/5314535331695962703.webp"
-            className="w-screen h-full relative"
-            alt=""
-          />
-
-          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col gap-y-10 bg-black/20 w-[749px]">
-            <div>
-              <span className="font-light text-white leading-8 text-xl uppercase">
-                Ибис маркетинг
-              </span>
-              <h1 className="leading-[58px] text-[50px] text-white">
-                Наружная реклама
-              </h1>
-            </div>
-            <div className="flex justify-center items-center gap-x-8">
-              <a href="https://smm-ibis.by/naruzhnayareklama/">
-                <button className="px-6 py-4 bg-white rounded-[100px] text-lg leading-6 uppercase">
-                  Узнать больше
-                </button>
-              </a>
-
-              <button
-                onClick={handleModal}
-                className="px-6 py-4 text-white bg-[#FF3A2D] rounded-[100px] text-lg leading-6 uppercase"
-              >
-                Начать продвижение
+                Оставить заявку
               </button>
             </div>
           </div>
