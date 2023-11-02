5fa78601f2585a5a750540dda0079e68d4a70d8e
Lyshka <53864647+Lyshka@users.noreply.github.com>
2023-11-02 15:54:51 +0300

    switch
    switch slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -57,80 +57,6 @@ const Slider = () => {
       >
         <div className="relative min-w-max h-[calc(100vh-96px)] duration-500 text-black flex justify-start items-center">
           <img
-            src="https://smm-ibis.by/wp-content/uploads/2023/08/Баннер-2.webp"
-            className="w-screen h-full relative object-cover"
-            alt=""
-          />
-
-          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col backdrop-blur gap-y-10 bg-black/[0.15] w-[749px]">
-            <div>
-              <span className="font-light text-white leading-8 text-xl uppercase">
-                Ибис маркетинг
-              </span>
-              <h1 className="leading-[58px] text-[50px] text-white">
-                Маркетинговый отдел на аутсорсе
-              </h1>
-            </div>
-            <p className="text-white leading-6 tracking-[1%]">
-              Уже Kia, JAC и JETOUR выбрали нашу услугу для своего успеха на
-              аутсорсе, всего за 1100 BYN. Начнем прямо сейчас?
-            </p>
-            <div className="flex justify-center items-center gap-x-8">
-              <button
-                onClick={handleModal}
-                className="px-6 py-4 text-white bg-[#FF3A2D] rounded-[100px] text-lg leading-6 uppercase"
-              >
-                Оставить заявку
-              </button>
-            </div>
-          </div>
-
-          <div className="flex w-full justify-between items-center absolute px-[238px]">
-            <div
-              onClick={() => a()}
-              className="w-8 h-8 cursor-pointer rounded-full flex justify-center items-center bg-black/50"
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 12L6 8L10 4"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-            <div
-              onClick={() => b()}
-              className="w-8 h-8 rounded-full cursor-pointer flex justify-center items-center bg-black/50"
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6 4L10 8L6 12"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-
-        <div className="relative min-w-max h-[calc(100vh-96px)] duration-500 text-black flex justify-start items-center">
-          <img
             src="https://smm-ibis.by/wp-content/uploads/2023/11/5.webp"
             className="w-screen h-full relative object-cover"
             alt=""
@@ -223,6 +149,80 @@ const Slider = () => {
               Наш профессиональный подход обеспечит стабильность и доступность
               вашего ресурса для посетителей, техподдержка сайта в нашей
               компании осуществляется высококвалифицированными специалистами.
+            </p>
+            <div className="flex justify-center items-center gap-x-8">
+              <button
+                onClick={handleModal}
+                className="px-6 py-4 text-white bg-[#FF3A2D] rounded-[100px] text-lg leading-6 uppercase"
+              >
+                Оставить заявку
+              </button>
+            </div>
+          </div>
+
+          <div className="flex w-full justify-between items-center absolute px-[238px]">
+            <div
+              onClick={() => a()}
+              className="w-8 h-8 cursor-pointer rounded-full flex justify-center items-center bg-black/50"
+            >
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 12L6 8L10 4"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div
+              onClick={() => b()}
+              className="w-8 h-8 rounded-full cursor-pointer flex justify-center items-center bg-black/50"
+            >
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 4L10 8L6 12"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative min-w-max h-[calc(100vh-96px)] duration-500 text-black flex justify-start items-center">
+          <img
+            src="https://smm-ibis.by/wp-content/uploads/2023/08/Баннер-2.webp"
+            className="w-screen h-full relative object-cover"
+            alt=""
+          />
+
+          <div className="absolute ml-[310px] p-10 rounded-2xl flex justify-start items-start flex-col backdrop-blur gap-y-10 bg-black/[0.15] w-[749px]">
+            <div>
+              <span className="font-light text-white leading-8 text-xl uppercase">
+                Ибис маркетинг
+              </span>
+              <h1 className="leading-[58px] text-[50px] text-white">
+                Маркетинговый отдел на аутсорсе
+              </h1>
+            </div>
+            <p className="text-white leading-6 tracking-[1%]">
+              Уже Kia, JAC и JETOUR выбрали нашу услугу для своего успеха на
+              аутсорсе, всего за 1100 BYN. Начнем прямо сейчас?
             </p>
             <div className="flex justify-center items-center gap-x-8">
               <button
